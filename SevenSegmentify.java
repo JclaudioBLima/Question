c66b9a45520a15630a9e0818fe73400aa4c94c71
Claudio Lima <140120786+JclaudioBLima@users.noreply.github.com>
2024-11-16 13:32:48 -0300

    Add files via upload
    Questao 3

--- SevenSegmentify.java
+++ SevenSegmentify.java
@@ -1,77 +1,73 @@
+package javateste;
 
 public class SevenSegmentify {
-   public static String sevenSegmentify(String time) {
-        // Representação ASCII para cada dígito
-        String[] digitos = {
-            " _ 
-| |
-|_|", // 0
-            "   
-  |
-  |", // 1
-            " _ 
- _|
-|_ ", // 2
-            " _ 
- _|
- _|", // 3
-            "   
-|_|
-  |", // 4
-            " _ 
-|_ 
- _|", // 5     
-            " _ 
-|_ 
-|_|", // 6
-            " _ 
-  |
-  |", // 7
-            " _ 
-|_|
-|_|", // 8
-            " _ 
-|_|
- _|", // 9
-        };
-        
-        // Representação ASCII para os dois pontos
-        String doisPontos = " . 
-   
- . ";
-        
-        // Dividindo o horário em componentes
-        char[] chars = time.toCharArray(); // Erro: esqueci de tratar casos como "01:05"
-        
-        // Strings para armazenar cada linha da exibição final
-        String linha1 = "", linha2 = "", linha3 = "";
-        
-        // Mapeia cada caractere para a representação ASCII
-        for (char ch : chars) {
-            if (Character.isDigit(ch)) {
-                int num = Character.getNumericValue(ch);
-                String[] partes = digitos[num].split("
-");
-                
-                // Adicionar cada linha
-                linha1 += partes[0] + " ";
-                linha2 += partes[1] + " ";
-                linha3 += partes[2] + " ";
-            } else if (ch == ':') {
-                String[] partes = doisPontos.split("
-");
-                linha1 += partes[0] + " ";
-                linha2 += partes[1] + " ";
-                linha3 += partes[2] + " ";
-            }
-        }
-        
-        // Combina todas as linhas em uma string final
-        String resultado = linha1 + "
-" + linha2 + "
-" + linha3; // Erro: adiciona um espaço extra no final de cada linha
-        
-        return resultado.trim(); // Corrigindo o erro do espaço extra
-    }
+  public static void main(String[] args) throws Exception {
+    NumberSegmentify numberSegmentify = new NumberSegmentify();
+
+    System.out.println(numberSegmentify.stringfyHour("11:32"));
+  }
 }
-    
+
+class NumberSegmentify {
+  private String[] one = { "  ", " |", " |" };
+  private String[] two = { "_ ", "_|", "|_" };
+  private String[] three = { "_ ", "_|", "_|" };
+
+  public String getLine(int number, int line) throws Exception {
+    String lineStr;
+    line = line - 1;
+    switch (number) {
+      case 1:
+        lineStr = one[line];
+        break;
+      case 2:
+        lineStr = two[line];
+        break;
+      case 3:
+        lineStr = three[line];
+        break;
+      default:
+        throw new Exception("Unexpected number");
+    }
+
+    return lineStr;
+  }
+
+  public String getLine(String number, int line) throws Exception {
+    var numberInt = Integer.parseInt(number);
+    return getLine(numberInt, line);
+  }
+
+  public String stringfyHour(String time) throws Exception {
+    var numbers = time.split("");
+    String[] hours = { numbers[0], numbers[1] };
+    String[] minutes = { numbers[3], numbers[4] };
+
+    StringBuilder line1 = new StringBuilder();
+    StringBuilder line2 = new StringBuilder();
+    StringBuilder line3 = new StringBuilder();
+
+    line1.append(this.getLine(hours[0], 1));
+    line1.append(this.getLine(hours[1], 1));
+    line1.append(" ");
+    line1.append(this.getLine(minutes[0], 1));
+    line1.append(this.getLine(minutes[1], 1));
+
+    line2.append(this.getLine(hours[0], 2));
+    line2.append(this.getLine(hours[1], 2));
+    line2.append(".");
+    line2.append(this.getLine(minutes[0], 2));
+    line2.append(this.getLine(minutes[1], 2));
+
+    line3.append(this.getLine(hours[0], 3));
+    line3.append(this.getLine(hours[1], 3));
+    line3.append(".");
+    line3.append(this.getLine(minutes[0], 3));
+    line3.append(this.getLine(minutes[1], 3));
+
+    return line1.toString() + "\n"
+        + line2.toString() + "\n"
+        + line3.toString() + "\n";
+
+  }
+}
